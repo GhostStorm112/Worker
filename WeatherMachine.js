1b8309dc394a35156a5ee67a6496141e2ad87053
112madgamer <ryleegeorge1@gmail.com>
2018-05-27 20:11:26 +1000

    URL for docker

--- WeatherMachine.js
+++ WeatherMachine.js
@@ -24,7 +24,7 @@ class WeatherMachine extends EventEmitter {
     }, options)
 
     this.cache = new RainCache({
-      storage: { default: new RainCache.Engines.RedisStorageEngine({ password: process.env.REDIS_PASS }) },
+      storage: { default: new RainCache.Engines.RedisStorageEngine({ host: process.env.REDIS_URL, password: process.env.REDIS_PASS }) },
       debug: false
     })
     this.rest = new SnowTransfer(process.env.TOKEN)
