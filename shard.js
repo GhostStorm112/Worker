5712264c237c3701afd8cc1f3eaa6dd9f3923630
112madgamer <ryleegeorge1@gmail.com>
2018-05-28 11:51:45 +0000

    Update shard.js

--- shard.js
+++ shard.js
@@ -2,8 +2,14 @@ class Shard {
   constructor (wm) {
     this.wm = wm
   }
-
   sendWS (t, packet) {
+    this.wm.connector.sendToGateway({
+      t: 'VOICE_STATE_UPDATE,
+      d: packet
+    })
+  }
+
+  oldws (t, packet) {
     this.wm.connector.sendToGateway({
       t: t,
       d: packet
