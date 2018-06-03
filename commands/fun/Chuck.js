4fd6e0884aa133873cfd06e334be1ffa03bfc4e1
112madgamer <ryleegeorge1@gmail.com>
2018-06-03 22:14:50 +1000

    Chuck added

---
+++ commands/fun/Chuck.js
@@ -0,0 +1,32 @@
+const Command = require('../../structures/Command')
+const axios = require('axios')
+class Chuck extends Command {
+  get name () {
+    return 'chuck'
+  }
+
+  get aliases () {
+    return ['chucknorris']
+  }
+
+  async run (event, args) {
+    axios.get('http://api.icndb.com/jokes/random')
+      .then(response => {
+        return this.client.rest.channel.createMessage(event.channel_id, args || {
+          embed: {
+            author: {
+              name: `${event.author.username}#${event.author.discriminator} (${event.author.id})`,
+              icon_url: `https://cdn.discordapp.com/avatars/${event.author.id}/${event.author.avatar}.webp`
+            },
+            description: `**${response.data.value.joke}** `,
+            image: {
+              url: `https://thumbs.gfycat.com/MadeupMarvelousAfricancivet-size_restricted.gif`
+            }
+
+          }
+        })
+      })
+  }
+}
+
+module.exports = Chuck
