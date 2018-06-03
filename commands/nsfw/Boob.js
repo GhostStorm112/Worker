b0a575aa8e29707e00ef205854d10898d0dad2e3
112madgamer <ryleegeorge1@gmail.com>
2018-06-03 22:52:00 +1000

    Added boob and butt nsfw commands

---
+++ commands/nsfw/Boob.js
@@ -0,0 +1,44 @@
+const Command = require('../../structures/Command')
+const axios = require('axios')
+
+class Boob extends Command {
+  get name () {
+    return 'boobs'
+  }
+
+  get aliases () {
+    return ['boobies']
+  }
+
+  async run (event, args) {
+    const channel = await this.client.rest.channel.getChannel(event.channel_id)
+    if (channel.nsfw) {
+      console.log(randomInt)
+      axios.get(`http://api.oboobs.ru/boobs/${randomInt(5000)}/1/rank`)
+        .then(response => {
+          let image = `http://media.oboobs.ru/${response.data[0].preview}`
+          return this.client.rest.channel.createMessage(event.channel_id, args || {
+            embed: {
+              author: {
+                name: `${event.author.username}#${event.author.discriminator} (${event.author.id})`,
+                icon_url: `https://cdn.discordapp.com/avatars/${event.author.id}/${event.author.avatar}.webp`
+              },
+              description: `**OO YAA** `,
+              image: {
+                url: image
+              }
+
+            }
+          })
+        })
+    } else {
+      return this.client.rest.channel.createMessage(channel.id, 'This is not a NSFW channel!')
+    }
+
+    function randomInt (high) {
+      return Math.floor(Math.random() * high)
+    }
+  }
+}
+
+module.exports = Boob
---
+++ commands/nsfw/Butt.js
@@ -0,0 +1,44 @@
+const Command = require('../../structures/Command')
+const axios = require('axios')
+
+class Butt extends Command {
+  get name () {
+    return 'butts'
+  }
+
+  get aliases () {
+    return ['butt']
+  }
+
+  async run (event, args) {
+    const channel = await this.client.rest.channel.getChannel(event.channel_id)
+    if (channel.nsfw) {
+      console.log(randomInt)
+      axios.get(`http://api.obutts.ru/butts/${randomInt(5000)}/1/rank`)
+        .then(response => {
+          let image = `http://media.obutts.ru/${response.data[0].preview}`
+          return this.client.rest.channel.createMessage(event.channel_id, args || {
+            embed: {
+              author: {
+                name: `${event.author.username}#${event.author.discriminator} (${event.author.id})`,
+                icon_url: `https://cdn.discordapp.com/avatars/${event.author.id}/${event.author.avatar}.webp`
+              },
+              description: `**OO YAA** `,
+              image: {
+                url: image
+              }
+
+            }
+          })
+        })
+    } else {
+      return this.client.rest.channel.createMessage(channel.id, 'This is not a NSFW channel!')
+    }
+
+    function randomInt (high) {
+      return Math.floor(Math.random() * high)
+    }
+  }
+}
+
+module.exports = Butt
