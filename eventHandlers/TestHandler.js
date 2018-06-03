dd73d07b4619794062b37ffef7ba5b339e15d638
112madgamer <ryleegeorge1@gmail.com>
2018-06-03 21:42:57 +1000

    Stop annoying log

--- eventHandlers/TestHandler.js
+++ eventHandlers/TestHandler.js
@@ -10,7 +10,7 @@ class TestHandler extends EventHandler {
   }
 
   handle (event) {
-    console.log(`PRESENCE -> @${event.user.id} [${event.status}]${event.game ? ` { name: ${event.game.name}, type: ${event.game.type} }` : ''}`)
+    // console.log(`PRESENCE -> @${event.user.id} [${event.status}]${event.game ? ` { name: ${event.game.name}, type: ${event.game.type} }` : ''}`)
   }
 }
 
