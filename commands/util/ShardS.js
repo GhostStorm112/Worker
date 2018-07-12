b6e1409d36a59479718bcd7455383caf73698a97
112madgamer <ryleegeorge1@gmail.com>
2018-07-12 23:22:36 +1000

    Clean and fixing shard command

--- commands/util/ShardS.js
+++ commands/util/ShardS.js
@@ -13,15 +13,17 @@ class ShardS extends Command {
   async run (event, args) {
     if (!this.client.isOwner(event.author.id)) { return }
     let message = []
-    message.push(`\`\`\`javascript\n`)
+    message.push('```js\n')
     message.push('==Shards==\n')
     axios.get(`http://${process.env.GW_HOST}:${process.env.GW_PORT}/shards/status`)
       .then(response => {
         for (var shard in response.data.shards) {
+          console.log(response.data.shards[shard].status)
           message.push(`Shard-${response.data.shards[shard].id} S: "${response.data.shards[shard].status}" R: "${response.data.shards[shard].seq}"\n`)
         }
-        message.push(`\n\`\`\``)
+        message.push('```')
         console.log(message)
+
         return this.client.rest.channel.createMessage(event.channel_id, message.toString())
       })
   }
--- index.js
+++ index.js
@@ -30,17 +30,11 @@ async function run () {
   client.log.info('Worker', 'Starting')
   await client.initialize()
   client.log.info('Worker', 'Ready')
-  /*   wm.on('messageCreate', data => {
-    return handleMessage(data)
-  }) */
 
   client.on('dblu', async () => {
     client.log.info('DBL', `Updated Discord Bot List with ${await client.cache.guild.getIndexCount()}`)
     dbl.postStats(await client.cache.guild.getIndexCount(), null, null)
   })
 }
-/* async function handleMessage (msg) {
-   wm.log.info('Message', `${msg.shard_id}:${msg.guild_id}:${msg.guild_id}:${msg.author.username}#${msg.author.discriminator}: ${msg.content}`)
-} */
 
 run().catch(error => client.log.error('STARTUP', error))
