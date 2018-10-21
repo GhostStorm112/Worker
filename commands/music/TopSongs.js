8e31c9318499cc5a79ebd8bf9bdaefb91297049e
112madgamer <ryleegeorge1@gmail.com>
2018-10-21 15:21:44 +1100

    Starting to cache songs

--- commands/music/TopSongs.js
+++ commands/music/TopSongs.js
@@ -11,18 +11,37 @@ class TopSongs extends Command {
   }
 
   async run (event, args) {
-    billboard('hot-100', (err, songs) => {
-      let message
+    const songs = await this.client.cache.storage.get('topsongs', {type: 'arr'})
+    if (songs) {
+      this.client.log.info('TOPS', 'Getting cached songs')
 
+      let message
+      message = '```js\n' + '==Billboard top 10==\n'
+
+      for( const song of songs){
+        if(song.rank){
+          message = message + `Rank: "${song.rank}" Title: "${song.title}" Artist: "${song.artist}"\n`
+        }
+      }
+      message = message + '```'
+
+      return this.client.rest.channel.createMessage(event.channel_id, message)
+    }
+    billboard('hot-100', (err, songs) => {
+      this.client.log.info('TOPS', 'Getting new songs')
+      let message
+      const rsongs = []
       if (err) this.client.log.error(err)
       message = '```js\n' + '==Billboard top 10==\n'
       for (const song of songs) {
         if (song.rank > 10) break
+        // rsongs.push(`Rank: "${song.rank}" Title: "${song.title}" Artist: "${song.artist}"\n`)
+        rsongs[song.rank] = { rank: song.rank, title: song.title, artist: song.artist }
         message = message + `Rank: "${song.rank}" Title: "${song.title}" Artist: "${song.artist}"\n`
       }
-
       message = message + '```'
-      this.client.log.info('topsongs', message)
+      // this.client.log.info('topsongs', message)
+      this.client.cache.storage.set('topsongs', rsongs)
       return this.client.rest.channel.createMessage(event.channel_id, message)
     })
   }
